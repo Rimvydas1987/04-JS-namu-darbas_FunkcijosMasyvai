1da1b35fcf3e40271a9ff8e89edf76cd321387fe
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-06-16 19:01:25 +0300

    3-uzduotis(end)

--- 2-uzduotis/script.js
+++ 2-uzduotis/script.js
@@ -6,7 +6,7 @@ function sakniesTraukimas() {
             if (Math.sqrt(sk) >= 0) {
                 console.log(Math.sqrt(sk));
             } else {
-                console.log("sudas gaunas")
+                console.log("Šaknis iš neigiamo skaičiaus negalima");
             }
            }
            //document.getElementById("rezultatas").innerHTML = saknis;
--- 3-uzduotis/script.js
+++ 3-uzduotis/script.js
@@ -1,1 +1,28 @@
 "use strict"
+function PridetiDiv(){
+   
+    let masyvas = [6, 2, 4, 3, 0, 5];
+    console.log(masyvas);
+    for(let i = 0; i< masyvas.length; i++) {        
+
+    let skaicius, skaitmuo;
+        skaicius = masyvas[i];
+    let indeksas = 0;
+        
+    while (skaicius != 0) {
+        skaitmuo = skaicius % 10;
+        indeksas = skaitmuo;
+        
+        skaicius = parseInt(skaicius / 10);
+    }
+        
+    for (let i=0; i<1; i++) {
+
+        let divElementas = document.createElement("div");
+        divElementas.classList.add("elementasIndex");
+        divElementas.innerText = "elementas - " + indeksas;
+
+        document.querySelector(".talpykla").append(divElementas);
+    }
+    }
+}
