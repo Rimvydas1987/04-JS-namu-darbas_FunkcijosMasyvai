253ec49488a36ecbf4900718cb7cdb42e4c4ef35
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-06-16 14:34:18 +0300

    Initial commit

---
+++ 1-uzduotis/script.js
@@ -0,0 +1,8 @@
+"use strict"
+function MasyvoPildymas() {
+    let masyvas = [1, 2, 'tekstas', 15, 155];
+    masyvas[masyvas.length] = document.getElementById("reiksme").value;
+    
+    console.log(masyvas);
+
+}
