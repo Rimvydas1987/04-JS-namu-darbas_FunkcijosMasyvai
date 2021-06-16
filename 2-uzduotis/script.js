cc1edb0aaa405be29da8c7b69baf1b878442e1af
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-06-16 15:00:23 +0300

    1-uzduotis

--- 2-uzduotis/script.js
+++ 2-uzduotis/script.js
@@ -1,1 +1,25 @@
 "use strict"
+
+function sakniesTraukimas(){
+
+let zingsniai = 0;
+
+
+for (let i=0; i<40; i++) {
+
+    let divElementas = document.createElement("div");
+    divElementas.classList.add("div-dydis");
+    divElementas.innerText = i + 1; // +1 todel kad numeruotų ne nuo 0 o nuo 1
+
+
+    if( (i+1) % 2 == 0 ) {
+        divElementas.classList.add("zalias");
+    }
+
+    document.querySelector(".conteiner").append(divElementas);
+
+    console.log(i);
+    zingsniai++;
+    
+}
+}
